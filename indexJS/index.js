381a4331f9d916a879c0f80fc10fbb107754b589
Taua Taiapa <ttaiapa52@gmail.com>
2024-02-07 15:03:41 +1300

    responsive changes

--- indexJS/index.js
+++ indexJS/index.js
@@ -3,40 +3,45 @@ document.addEventListener('DOMContentLoaded', function () {
   const slideMenu = document.getElementById('slideMenu')
   let menuTimer
 
+  // Show slideMenu on mouseenter
   slideMenuButton.addEventListener('mouseenter', function () {
     clearTimeout(menuTimer)
     slideMenu.style.display = 'block'
   })
 
+  // Hide slideMenu on mouseleave after 3 seconds
   slideMenuButton.addEventListener('mouseleave', function () {
-    menuTimer = setTimeout(function () {
-      slideMenu.style.display = 'none'
-    }, 3000)
+    menuTimer = setTimeout(hideSlideMenu, 3000)
   })
 
+  // Clear timeout on slideMenu mouseenter
   slideMenu.addEventListener('mouseenter', function () {
     clearTimeout(menuTimer)
   })
 
+  // Hide slideMenu on slideMenu mouseleave after 3 seconds
   slideMenu.addEventListener('mouseleave', function () {
-    menuTimer = setTimeout(function () {
-      slideMenu.style.display = 'none'
-    }, 3000)
+    menuTimer = setTimeout(hideSlideMenu, 3000)
   })
-})
 
-document.addEventListener('DOMContentLoaded', function () {
+  // Handle links with fade-out effect
   const links = document.querySelectorAll('.nav-link')
 
   links.forEach((link) => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault()
-
-      document.body.classList.add('fade-out')
-
-      setTimeout(() => {
-        window.location.href = link.href
-      }, 3000)
-    })
+    link.addEventListener('click', handleLinkClick)
   })
+
+  function handleLinkClick(event) {
+    event.preventDefault()
+
+    document.body.classList.add('fade-out')
+
+    setTimeout(() => {
+      window.location.href = event.target.href
+    }, 3000)
+  }
+
+  function hideSlideMenu() {
+    slideMenu.style.display = 'none'
+  }
 })
