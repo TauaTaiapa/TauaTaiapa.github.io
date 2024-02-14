fa68a21e59189651d0a6728f936f41fbdde25f9d
Taua Taiapa <ttaiapa52@gmail.com>
2024-02-14 15:21:44 +1300

    New changes to responsiveness

--- indexJS/index.js
+++ indexJS/index.js
@@ -3,28 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
   const slideMenu = document.getElementById('slideMenu')
   let menuTimer
 
-  // Show slideMenu on mouseenter
-  slideMenuButton.addEventListener('mouseenter', function () {
+  slideMenuButton.addEventListener('click', function () {
     clearTimeout(menuTimer)
     slideMenu.style.display = 'block'
   })
 
-  // Hide slideMenu on mouseleave after 3 seconds
-  slideMenuButton.addEventListener('mouseleave', function () {
+  slideMenuButton.addEventListener('', function () {
     menuTimer = setTimeout(hideSlideMenu, 3000)
   })
 
-  // Clear timeout on slideMenu mouseenter
-  slideMenu.addEventListener('mouseenter', function () {
+  slideMenu.addEventListener('click', function () {
     clearTimeout(menuTimer)
   })
 
-  // Hide slideMenu on slideMenu mouseleave after 3 seconds
-  slideMenu.addEventListener('mouseleave', function () {
+  slideMenu.addEventListener('', function () {
     menuTimer = setTimeout(hideSlideMenu, 3000)
   })
 
-  // Handle links with fade-out effect
   const links = document.querySelectorAll('.nav-link')
 
   links.forEach((link) => {
